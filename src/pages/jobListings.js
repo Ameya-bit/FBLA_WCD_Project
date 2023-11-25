9ead9e806a9f1e26f1c6d356a6f44e45ef7cf345
Ameya Panchal <54782629+Ameya-bit@users.noreply.github.com>
2023-11-25 19:23:58 +0000

    idk

--- src/pages/jobListings.js
+++ src/pages/jobListings.js
@@ -7,31 +7,56 @@ import data from "../data/jobs.json";
 const queryParameters = new URLSearchParams(window.location.search)
 const i = queryParameters.get("jobNum")
 const jobListings = () => {
-  return (
-    <div class="main ">
-      <SpacingCont amount="5" />
-      <h2 class="d-flex justify-content-center">Search for Jobs</h2>
-      <SpacingCont amount="1" />
-      <div class="card-columns d-flex justify-content-center padd">
-        <div class="shaded container round col-4 padd">
-          <h1 class="d-flex justify-content-center padd">Filters: </h1>
-          <div class="padd card-columns">
-            <Inputs name="Category" type="text"/>
-            <Inputs name="Location" type="text"/>
-            <Inputs name="Type" type="text"/>
+  if(i == undefined)
+    {
+        return(
+            <div class="main padd">
+                <SpacingCont amount="14" />
+                <div class="container shaded round col-8  ">
+                    <SpacingCont amount="2" />
+                    <h3 class="d-flex justify-content-center">
+                    How'd you get here?
+                    </h3>
+                    <h2 class="d-flex justify-content-center">
+                    ¯\_(ツ)_/¯
+                    </h2>
+                    <h3 class="d-flex justify-content-center">
+                        Click on the navbar to get back on track
+                    </h3>
+                    <SpacingCont amount="2" />
+                </div>
+                <SpacingCont amount="14" /> 
+            </div>
+        )
+    }
+  else{
+    return (
+      <div class="main ">
+        <SpacingCont amount="5" />
+        <h2 class="d-flex justify-content-center">Search for Jobs</h2>
+        <SpacingCont amount="1" />
+        <div class="card-columns d-flex justify-content-center padd">
+          <div class="shaded container round col-4 padd">
+            <h1 class="d-flex justify-content-center padd">Filters: </h1>
+            <div class="padd card-columns">
+              <Inputs name="Category" type="text"/>
+              <Inputs name="Location" type="text"/>
+              <Inputs name="Type" type="text"/>
+            </div>
+            
+            <div id="jobslist" class=" container round border scroll padd">
+              <JobPush end="12"/>
+            </div>
           </div>
-          
-          <div id="jobslist" class=" container round border scroll padd">
-            <JobPush end="12"/>
-          </div>
-        </div>
-      
-        <JobFullData />
         
+          <JobFullData />
+          
+        </div>
+        <SpacingCont amount="5" />
       </div>
-      <SpacingCont amount="5" />
-    </div>
-  );
+    );
+  }
+  
 };
 
 function JobFullData(){
