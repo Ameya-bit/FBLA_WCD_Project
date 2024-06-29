5875593cc37294f6991a14fa41029232affb35f2
Ameya-bit <ameyapanchal011@gmail.com>
2024-06-29 16:15:34 -0400

    date fix final hopefully

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -58,8 +58,8 @@ const Apply = () => {
   }
 
   function resumeDue() {
-    var day = date.getDate() + 5 > 30 ? date.getDate() - 26 : date.getDate + 5;
     var month = day > 30 ? date.getMonth() + 2 : date.getMonth() + 1;
+    var day = date.getDate() + 5 > 30 ? date.getDate() - 26 : date.getDate + 5;
     var year = date.getFullYear();
 
     var fullDate =
